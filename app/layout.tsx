d59a44ad5d5811368090576a5f6cbddf7debdf61
Tribhuwan <mathincosmos@gmail.com>
2023-10-30 13:11:19 +0530

    create skeleton components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: 'Travel',
-  description: 'Travel App for Camping',
-}
+  title: "Travel",
+  description: "Travel App for Camping",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar /> {children} <Footer />
+      </body>
     </html>
-  )
+  );
 }
