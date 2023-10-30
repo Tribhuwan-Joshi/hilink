a6f09bce36c6d812ba5826463b0f2112e63a2cd8
Tribhuwan <mathincosmos@gmail.com>
2023-10-30 14:47:57 +0530

    design home section

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,12 +4,15 @@ type ButtonProps = {
   type: "button" | "submit";
   title: string;
   icon?: string; // ? mark on property means optional
-  variant: "btn_dark_green";
+  variant: string;
 };
 
 const Button = ({ type, title, icon, variant }: ButtonProps) => {
   return (
-    <button type={type} className={`flexCenter gap-3 rounded-full border ${variant} `}>
+    <button
+      type={type}
+      className={`flexCenter gap-3 rounded-full border ${variant} `}
+    >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <label className="bold-16">{title}</label>
     </button>
